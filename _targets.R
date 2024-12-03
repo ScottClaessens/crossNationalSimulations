4d262a64c58a2d4ddc358675734d4199d4902a57
Scott Claessens <scott.claessens@gmail.com>
2024-12-03 17:10:19 +0000

    Initial commit

---
+++ _targets.R
@@ -0,0 +1,15 @@
+library(targets)
+
+# pipeline
+list(
+  # proximity matrix files
+  tar_target(geographic_proximity_file,    "data/geographic_proximity.rds"),
+  tar_target(linguistic_proximity_file,    "data/linguistic_proximity.rds"),
+  tar_target(religious_proximity_file,     "data/religious_proximity.rds"),
+  tar_target(religious_proximity_dow_file, "data/religious_proximity_dow.rds"),
+  # load proximity matrices
+  tar_target(geographic_proximity,    readRDS(geographic_proximity_file)),
+  tar_target(linguistic_proximity,    readRDS(linguistic_proximity_file)),
+  tar_target(religious_proximity,     readRDS(religious_proximity_file)),
+  tar_target(religious_proximity_dow, readRDS(religious_proximity_dow_file))
+)
